72efe4bfd9dc49db6a9f0b81af3cd74f84ed23e5
marianasuazas <125661779+marianasuazas@users.noreply.github.com>
2023-03-12 14:52:55 -0500

    commit country

---
+++ Stores.API/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stores.Shared.Entities;
+
+namespace Stores.API.Data
+{
+    //HERENCIA CONECION BASE DE DATOS
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        //OVERRAIDE MODEL
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); //MAPEO ENTIDADES
+
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+
+        }
+    }
+}
---
+++ Stores.Shared/Entities/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stores.Shared.Entities
+{
+    public class Country
+    {// primera tabla PAIS ACA SE CREAN SIEMPRE LAS ENTIDADES
+        public int Id { get; set; }
+
+
+        [Display(Name = "Pais")]
+        [MaxLength(100, ErrorMessage = "Cuidado el campo {0} solo puede contener {1} caracteres")]
+        [Required(ErrorMessage = "*el campo {0} es obligatorio ")]
+
+        public string Name { get; set; } = null;
+
+    }
+}
